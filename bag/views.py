1fd6d00977ac805e5a7393e9581d258028e6def0
Natalija Bujevic <natalijabujevic0708@gmail.com>
2021-02-17 22:42:29 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -2,6 +2,8 @@ import io, sys, pyautogui
 
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.contrib import messages
+
 from products.models import Product
 from .models import Design
 
@@ -43,15 +45,17 @@ def add_to_bag(request, item_id):
     bag[item_id] = [quantity, design.pk]
     request.session['bag'] = bag
     
+    messages.success(request, f'Added {product.name} to your bag')
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
     bag[item_id][0] = quantity
+    messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
@@ -59,9 +63,12 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
     try:
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
         request.session['bag'] = bag
+        messages.success(request, f'Removed {product.name} from your bag')
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
