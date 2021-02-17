bc0efa19f04ab82c53a1cd0071c039ee17b4ead4
Natalija Bujevic <natalijabujevic0708@gmail.com>
2021-02-17 22:42:53 +0000

    Add sorting of the products by price and category

--- products/views.py
+++ products/views.py
@@ -18,12 +18,30 @@ def product_categories(request):
 
 def all_products(request):
     """ A view to show products """
-
+    categories = Category.objects.all()
     products = Product.objects.all()
     query = None
+    sort = None
+    direction = None
+    if 'sort' in request.GET:
+        sortkey = request.GET['sort']
+        sort = sortkey
+        if sortkey == 'name':
+            sortkey = 'lower_name'
+            products = products.annotate(lower_name=Lower('name'))
+            
+
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+        products = products.order_by(sortkey)
     if 'category' in request.GET:
         categories = [request.GET['category']]
         products = products.filter(category__name__in=categories)
+        categories = Category.objects.all()
+        
+
     if 'q' in request.GET:
         query = request.GET['q']
         if not query:
@@ -32,9 +50,12 @@ def all_products(request):
 
         queries = Q(name__icontains=query) | Q(description__icontains=query)
         products = products.filter(queries)
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
