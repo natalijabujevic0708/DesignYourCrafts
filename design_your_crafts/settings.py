808b772bbe3a011cf16730dcf66c1d9ba0d1d0d2
Natalija Bujevic <natalijabujevic0708@gmail.com>
2021-02-21 16:59:03 +0000

    Updated settings.py

--- design_your_crafts/settings.py
+++ design_your_crafts/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
+from Xlib.display import Display
+disp = Display(":0.0")
+root = disp.screen().root
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
